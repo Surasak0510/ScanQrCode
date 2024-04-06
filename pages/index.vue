31ee5a42ee99da8684f9cf7cd9f7b2bc09c88c56
Surasak <92101184+Surasak0510@users.noreply.github.com>
2024-04-07 02:18:00 +0700

    Add button

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-12 d-flex mx-auto">
+          <a href="/qr-scanner" type="button" class="btn btn-primary mx-auto">Camera</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
--- pages/qr-scanner.vue
+++ pages/qr-scanner.vue
@@ -1,62 +1,68 @@
 <template>
-    <div class="container d-flex">
-      <div id="reader" class="mx-auto m-5"></div>
-      <div id="result" v-if="scannedData">
-        <h2>Success!</h2>
-        <p>
-          <a :href="scannedData" target="_blank">{{ scannedData }}</a>
-        </p>
+  <div class="container">
+    <div class="row d-flex mt-5 mb-3">
+      <a href="/" type="button" class="btn btn-primary mx-auto">Home</a>
+    </div>
+    <div class="row d-flex">
+      <div class="col-12">
+        <div id="reader" class="mx-auto"></div>
+        <div id="result" v-if="scannedData">
+          <h2>Success!</h2>
+          <p>
+            <a :href="scannedData" target="_blank">{{ scannedData }}</a>
+          </p>
+        </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import { Html5QrcodeScanner } from 'html5-qrcode';
-  
-  export default {
-    data() {
-      return {
-        scannedData: null,
-      };
+  </div>
+</template>
+
+<script>
+import { Html5QrcodeScanner } from 'html5-qrcode';
+
+export default {
+  data() {
+    return {
+      scannedData: null,
+    };
+  },
+  mounted() {
+    const scanner = new Html5QrcodeScanner(
+      'reader',
+      {
+        qrbox: { width: 250, height: 250 },
+        fps: 20,
+      }
+    );
+
+    scanner.render(this.onScanSuccess, this.onScanError);
+  },
+  methods: {
+    onScanSuccess(result) {
+      this.scannedData = result;
+      // Optionally stop scanning after a successful scan
+      scanner.clear();
     },
-    mounted() {
-      const scanner = new Html5QrcodeScanner(
-        'reader',
-        {
-          qrbox: { width: 250, height: 250 },
-          fps: 20,
-        }
-      );
-  
-      scanner.render(this.onScanSuccess, this.onScanError);
+    onScanError(err) {
+      // console.error(err);
     },
-    methods: {
-      onScanSuccess(result) {
-        this.scannedData = result;
-        // Optionally stop scanning after a successful scan
-        scanner.clear();
-      },
-      onScanError(err) {
-        // console.error(err);
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  #reader {
-    width: 600px;
-  }
-  
-  #result {
-    text-align: center;
-    font-size: 1.5rem;
-  }
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#reader {
+  width: 600px;
+}
+
+#result {
+  text-align: center;
+  font-size: 1.5rem;
+}
+</style>
